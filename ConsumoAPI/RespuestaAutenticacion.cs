9d349e776ef5545771e51dde510c1705d2c80c9d
MarceloBas <marcelotorresmail@gmail.com>
2023-08-08 18:46:24 -0300

    Agregar archivos de proyecto.

---
+++ ConsumoAPI/RespuestaAutenticacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumoAPI
+{
+    public class RespuestaAutenticacion
+    {
+        public string token { get; set; }
+        public DateTime expiracion { get; set; }
+        public IList<string>? roles { get; set; }
+        public string errorResp { get; set; }
+
+    }
+}
