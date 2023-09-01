c36353e15120b1bc0c3bce9a5f332750df793fd1
MarceloBas <marcelotorresmail@gmail.com>
2023-09-01 14:51:33 -0300

    metodo con webrequest comentado

---
+++ ConsumoAPI/CredencialesUsuarioDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumoAPI
+{
+
+    public class CredencialesUsuarioDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+
+        [Required]
+        public string password { get; set; }
+
+        //public string? nombre { get; set; }
+
+        //public string? apellido { get; set; }
+    }
+
+}
--- ConsumoAPI/Program.cs
+++ ConsumoAPI/Program.cs
@@ -13,45 +13,39 @@ Console.WriteLine("Introduzca un catalogo");
 string catalogo;
 catalogo = Console.ReadLine();
 
-Console.WriteLine("Stock: " + await GetstockxCatalogo(GetToken(), catalogo));// "G  -052-545-K9"));
+Console.WriteLine("Stock: " + await GetstockxCatalogo(await GetToken(), catalogo));// "G  -052-545-K9"));
 Console.ReadKey();
 
 
-    static string GetToken()
+    static async Task<string> GetToken()
 {
-    //Ejemplo con WebRequest(a partir de net 6 esta marcada como obsoleto)
-    var url = $"http://oliauto.dyndns.org:2088/api/Acceso/Acceso";
-    var request = (HttpWebRequest)WebRequest.Create(url);
-    string json = $"{{\"email\":\"UserTest@Test.com.ar\",\"password\":\"Aa1234!\"}}";
-    request.Method = "POST";
-    request.ContentType = "application/json";
-    request.Accept = "application/json";
-
-    using (var streamWriter = new StreamWriter(request.GetRequestStream()))
-    {
-        streamWriter.Write(json);
-        streamWriter.Flush();
-        streamWriter.Close();
-    }
-
+    
+    var url = $"http://oliauto.dyndns.info:2088/api/Acceso/Acceso";
+    CredencialesUsuarioDTO credenciales = new CredencialesUsuarioDTO() { email = "UserTest@Test.com.ar", password = "Aa1234!" };
     try
     {
-        using (WebResponse response = request.GetResponse())
+        using (var client_ = new HttpClient())
         {
-            using (Stream strReader = response.GetResponseStream())
+            using (var request_ = new HttpRequestMessage())
             {
-                if (strReader == null) return "";
-                using (StreamReader objReader = new StreamReader(strReader))
+                request_.Content = new StringContent(JsonSerializer.Serialize(credenciales));
+                request_.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+                request_.Method = new HttpMethod("POST");
+                request_.RequestUri = new Uri(url);
+                request_.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
+
+                var response_ = await client_.SendAsync(request_, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
+                var responseText_ = await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                if (response_.StatusCode == System.Net.HttpStatusCode.OK) // 200
                 {
-                    string responseBody = objReader.ReadToEnd();
 
-                    RespuestaAutenticacion? respuesta =JsonSerializer.Deserialize<RespuestaAutenticacion>(responseBody);
+                    var responseBody_ = JsonSerializer.Deserialize<RespuestaAutenticacion>(responseText_);
 
-                    //Console.WriteLine("Token = " + respuesta.token);
-                    //Console.WriteLine("Expiracion = " + respuesta.expiracion);
-                    //Console.WriteLine("");
-
-                    return respuesta.token;
+                    return responseBody_.token.ToString();
+                }
+                else
+                {
+                    return response_.RequestMessage.ToString();
                 }
             }
         }
@@ -60,11 +54,53 @@ Console.ReadKey();
     {
         return "ERROR : " + ex.Message;
     }
+    #region "metodo obsoleto para net 6
+    //Ejemplo con WebRequest(a partir de net 6 esta marcada como obsoleto)
+    //var request = (HttpWebRequest)WebRequest.Create(url);
+    //string json = $"{{\"email\":\"UserTest@Test.com.ar\",\"password\":\"Aa1234!\"}}";
+    //request.Method = "POST";
+    //request.ContentType = "application/json";
+    //request.Accept = "application/json";
+
+    //using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+    //{
+    //    streamWriter.Write(json);
+    //    streamWriter.Flush();
+    //    streamWriter.Close();
+    //}
+
+    //try
+    //{
+    //    using (WebResponse response = request.GetResponse())
+    //    {
+    //        using (Stream strReader = response.GetResponseStream())
+    //        {
+    //            if (strReader == null) return "";
+    //            using (StreamReader objReader = new StreamReader(strReader))
+    //            {
+    //                string responseBody = objReader.ReadToEnd();
+
+    //                RespuestaAutenticacion? respuesta = JsonSerializer.Deserialize<RespuestaAutenticacion>(responseBody);
+
+    //                //Console.WriteLine("Token = " + respuesta.token);
+    //                //Console.WriteLine("Expiracion = " + respuesta.expiracion);
+    //                //Console.WriteLine("");
+
+    //                return respuesta.token;
+    //            }
+    //        }
+    //    }
+    //}
+    //catch (WebException ex)
+    //{
+    //    return "ERROR : " + ex.Message;
+    //}
+    #endregion
 }
 
 static async Task<string>  GetstockxCatalogo(string token, string catalogo)
 {
-    var url = $"http://oliauto.dyndns.org:2088/api/Encuesta/StockxCatalogo";
+    var url = $"http://oliauto.dyndns.info:2088/api/Encuesta/StockxCatalogo";
 
     DateTime localDate = DateTime.Now;
     string dia = localDate.ToString();
